c2400ed3210acd91d896b68ce72a56bc68302522
Praveen Nagar <praveenshe6@gmail.com>
2024-01-27 21:54:00 +0530

    Second commit

---
+++ App.js
@@ -0,0 +1,48 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+
+const button = document.getElementById("addButton");
+// console.log(button);
+
+button.addEventListener("click", () =>{
+    if(inputBox.value == ""){
+        alert("please write some text");
+    }
+
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span); 
+    }
+    inputBox.value = "";
+    saveData();
+})
+
+
+
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
